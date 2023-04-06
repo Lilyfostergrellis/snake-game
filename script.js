be0138ee96e64682d11760d2a34e58b45d5a830f
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-06 15:51:13 +0100

    snake head moves on keyboard direction press

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const playBoard = document.querySelector(".play-board");
 
 let foodX = 13, foodY = 10;
 let snakeX = 5, snakeY = 15;
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPositioning = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
@@ -9,13 +10,43 @@ const changeFoodPositioning = () => {
     //passes a random value between 0-30 as the food position
 }
 
+const changeDirection = (e) => {
+    if(e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if(e.key === "ArrowDown"){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if(e.key === "ArrowLeft"){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if(e.key === "ArrowRight"){
+        velocityX = 1;
+        velocityY = 0;
+    }
+    //change velocity based on user key press
+    initGame();
+
+}
+
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+    //update to position of snake's head based on current velocity on game-board
+
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
+
 
 changeFoodPositioning();
 //on refresh of browser, the food position changes randomly
 initGame();
 //once initGame is called, 'food' and 'snake-head' is rendered
+
+document.addEventListener("keydown", changeDirection);
