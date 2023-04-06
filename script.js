f480a7f9c13ef6b21502c3ac53fb4d476b1dff2e
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-06 15:25:21 +0100

    random relocation of food on refresh of browser

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const playBoard = document.querySelector(".play-board");
 
 let foodX = 13, foodY = 10;
 
+const changeFoodPositioning = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+    //passes a random value between 0-30 as the food position
+}
+
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 
+changeFoodPositioning();
+//on refresh of browser, the food position changes randomly
 initGame();
 //once initGame is called, 'food' is rendered
